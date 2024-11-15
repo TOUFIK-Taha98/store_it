d815704ab56c9970db79c54a1bb464c959ee3f14
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2024-11-15 11:33:05 +0100

    Sign up server action

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,78 @@
+// ** Create account flow **
+
+// 0. Using the "use server" to not expose secret key
+// 1. User enters full name and email
+// 2. Check if the user already exist using the email (i will use this to identify if i still need to create a user document or not)
+// 3. Send OTP to user(s email)
+// 4. This will send a secret key for creating a session.
+// 5. Create a new user document if the user is a new user
+// 6. Return the user's accountId that will be used to complete the login
+// 7. Verify OTP and authenticate to login
+
+"use server";
+
+import { ID, Query } from "node-appwrite";
+import { createAdminClient } from "../appwrite";
+import { appwriteConfig } from "../appwrite/config";
+import { parseStringify } from "../utils";
+
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
+
+  const result = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersCollectionId,
+    [Query.equal("email", [email])]
+  );
+
+  return result.total > 0 ? result.documents[0] : null;
+};
+
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
+
+const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createAdminClient();
+
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+    return session.userId;
+  } catch (error) {
+    handleError(error, "Failed to send Email OTP");
+  }
+};
+
+export const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  const existingUser = await getUserByEmail(email);
+
+  const accountId = await sendEmailOTP({ email });
+
+  if (!accountId) throw new Error("Failed to send an OTP");
+
+  if (!existingUser) {
+    const { databases } = await createAdminClient();
+
+    await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollectionId,
+      ID.unique(),
+      {
+        fullName,
+        email,
+        avatar:
+          "https://static.vecteezy.com/system/resources/previews/021/548/095/large_2x/default-profile-picture-avatar-user-avatar-icon-person-icon-head-icon-profile-picture-icons-default-anonymous-user-male-and-female-businessman-photo-placeholder-social-network-avatar-portrait-free-vector.jpg",
+        accountId,
+      }
+    );
+  }
+
+  return parseStringify({ accountId });
+};
