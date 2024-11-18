c37e7aced379e74d202e70509eec0ab564da0a31
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2024-11-18 14:42:02 +0100

    Layouts & Header Layout

---
+++ components/Header.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+import Search from "./Search";
+import FileUploader from "./FileUploader";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Search />
+      <div className="header-wrapper">
+        <FileUploader />
+        <form>
+          <Button type="submit" className="sign-out-button">
+            <Image
+              src={"/assets/icons/logout.svg"}
+              alt="logo"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default Header;
