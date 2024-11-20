17fef2cc8eedaa08ccb6852c286f7701e9187d44
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2024-11-21 00:28:01 +0100

    file action

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,4 +1,30 @@
-const page = () => {
-  return <div>page</div>;
+import Sort from "@/components/Sort";
+
+const Page = async ({ params }: SearchParamProps) => {
+  const type = ((await params)?.type as string) || "";
+
+  return (
+    <div className="page-container">
+      <section className="w-full">
+        <h1 className="h1 capitalize">{type}</h1>
+
+        <div className="total-size-section">
+          <p className="body-1">
+            Total : <span className="h5">0 MB</span>
+          </p>
+
+          <div className="sort-container">
+            {" "}
+            <p className="body-1 hidden text-light-200 sm:block">
+              {" "}
+              Sort by: <Sort />{" "}
+            </p>
+          </div>
+        </div>
+      </section>
+
+      {/* Render the files dynamically */}
+    </div>
+  );
 };
-export default page;
+export default Page;
--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -5,8 +5,10 @@ import { appwriteConfig } from "@/lib/appwrite/config";
 import { constructFileUrl, getFileType, parseStringify } from "@/lib/utils";
 import { File } from "buffer";
 import { revalidatePath } from "next/cache";
-import { ID } from "node-appwrite";
+import { ID, Models, Query } from "node-appwrite";
 import { InputFile } from "node-appwrite/file";
+
+import { getCurrentUser } from "./user.actions";
 
 const handleError = (error: unknown, message: string) => {
   console.log(error, message);
@@ -64,5 +66,62 @@ export const uploadFile = async ({
   } catch (error) {
     console.error("Upload error details:", error);
     handleError(error, "Failed to upload file");
+  }
+};
+
+const createQueries = (
+  currentUser: Models.Document,
+  types: string[],
+  searchText: string,
+  sort: string,
+  limit?: number
+) => {
+  const queries = [
+    Query.or([
+      Query.equal("owner", [currentUser.$id]),
+      Query.contains("users", [currentUser.email]),
+    ]),
+  ];
+
+  if (types.length > 0) queries.push(Query.equal("type", types));
+  if (searchText) queries.push(Query.contains("name", searchText));
+  if (limit) queries.push(Query.limit(limit));
+
+  if (sort) {
+    const [sortBy, orderBy] = sort.split("-");
+
+    queries.push(
+      orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy)
+    );
+  }
+
+  return queries;
+};
+
+export const getFiles = async ({
+  limit,
+  searchText = "",
+  sort = "$createdAt-desc",
+  types = [],
+}: GetFilesProps) => {
+  const { databases } = await createAdminClient();
+
+  try {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) throw new Error("User not found");
+
+    const queries = createQueries(currentUser, types, searchText, sort, limit);
+
+    const files = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.filesCollectionId,
+      queries
+    );
+
+    console.log({ files });
+    return parseStringify(files);
+  } catch (error) {
+    handleError(error, "Failed to get files");
   }
 };
