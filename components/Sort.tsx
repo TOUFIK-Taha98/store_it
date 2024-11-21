00f0a2ab6d1a1fd8e203bbc3588804a09ae265e9
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2024-11-21 09:54:58 +0100

    Sort

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,4 +1,41 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { sortTypes } from "@/constants";
+import { usePathname, useRouter } from "next/navigation";
+
 const Sort = () => {
-  return <div>Sort</div>;
+  const path = usePathname();
+  const router = useRouter();
+
+  const handleSort = (value: string) => {
+    router.push(`${path}?sort=${value}`);
+  };
+
+  return (
+    <Select defaultValue={sortTypes[0].value} onValueChange={handleSort}>
+      <SelectTrigger className="sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent className="sort-select-content">
+        {sortTypes.map((sort) => (
+          <SelectItem
+            className="shad-select-item"
+            key={sort.label}
+            value={sort.value}
+          >
+            {sort.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
 };
+
 export default Sort;
