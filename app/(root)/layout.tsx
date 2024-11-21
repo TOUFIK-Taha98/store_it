1276c14ad45e7f08dfb88c121c761ddc04cd8caf
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2024-11-21 09:51:20 +0100

    Filter files & global search

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,10 @@
 import Header from "@/components/Header";
 import MobileNavigation from "@/components/MobileNavigation";
 import Sidebar from "@/components/Sidebar";
+import { Toaster } from "@/components/ui/toaster";
 import { getCurrentUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
 import React from "react";
-import { Toaster } from "@/components/ui/toaster";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
@@ -15,7 +15,7 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
       <Sidebar {...currentUser} />
       <section className="flex h-full flex-1 flex-col">
         <MobileNavigation {...currentUser} />{" "}
-        <Header userId={currentUser.$id} accountId={currentUser.accountId} />
+        <Header accountId={currentUser.accountId} userId={currentUser.$id} />
         <div className="main-content">{children}</div>
       </section>
       <Toaster />
--- components/Search.tsx
+++ components/Search.tsx
@@ -1,4 +1,106 @@
+"use client";
+
+import FormattedDateTime from "@/components/FormattedDateTime";
+import Thumbnail from "@/components/Thumbnail";
+import { Input } from "@/components/ui/input";
+import { getFiles } from "@/lib/actions/file.actions";
+import Image from "next/image";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Models } from "node-appwrite";
+import React, { useEffect, useState } from "react";
+import { useDebounce } from "use-debounce";
 const Search = () => {
-  return <div>Search</div>;
+  const [query, setQuery] = useState("");
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("query") || "";
+  const [results, setResults] = useState<Models.Document[]>([]);
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const path = usePathname();
+  const [debouncedQuery] = useDebounce(query, 300);
+
+  useEffect(() => {
+    const fetchFiles = async () => {
+      if (debouncedQuery.length === 0) {
+        setResults([]);
+        setOpen(false);
+        return router.push(path.replace(searchParams.toString(), ""));
+      }
+
+      const files = await getFiles({ searchText: debouncedQuery, types: [] });
+      setResults(files.documents);
+      setOpen(true);
+    };
+
+    fetchFiles();
+  }, [debouncedQuery]);
+
+  useEffect(() => {
+    if (!searchQuery) {
+      setQuery("");
+    }
+  }, [searchQuery]);
+
+  const handleClickItem = (file: Models.Document) => {
+    setOpen(false);
+    setResults([]);
+
+    router.push(
+      `/${file.type === "video" || file.type === "audio" ? "media" : file.type + "s"}?query=${query}`
+    );
+  };
+
+  return (
+    <div className="search">
+      <div className="search-input-wrapper">
+        <Image
+          alt="Search"
+          height={24}
+          src="/assets/icons/search.svg"
+          width={24}
+        />
+        <Input
+          className="search-input"
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search..."
+          value={query}
+        />
+
+        {open && (
+          <ul className="search-result">
+            {results.length > 0 ? (
+              results.map((file) => (
+                <li
+                  className="flex items-center justify-between"
+                  key={file.$id}
+                  onClick={() => handleClickItem(file)}
+                >
+                  <div className="flex cursor-pointer items-center gap-4">
+                    <Thumbnail
+                      className="size-9 min-w-9"
+                      extension={file.extension}
+                      type={file.type}
+                      url={file.url}
+                    />
+                    <p className="subtitle-2 line-clamp-1 text-light-100">
+                      {file.name}
+                    </p>
+                  </div>
+
+                  <FormattedDateTime
+                    className="caption line-clamp-1 text-light-200"
+                    date={file.$createdAt}
+                  />
+                </li>
+              ))
+            ) : (
+              <p className="empty-result">No files found</p>
+            )}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 };
+
 export default Search;
